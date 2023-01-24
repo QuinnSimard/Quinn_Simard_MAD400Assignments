2df0e34e84d18d52e3ce2eaafac73b64ac90d872
Quinn Simard <w0643876@myscc.ca>
2023-01-23 23:03:41 -0500

    step 11 completed - modified attribute and creating methods for the classes

--- Q_Simard_Clothing/src/app/models/content-list.ts
+++ Q_Simard_Clothing/src/app/models/content-list.ts
@@ -1,2 +1,27 @@
+import { IContent } from "./icontent";
+
 export class ContentList {
+
+private _hoodies: IContent[];
+
+constructor(){
+    this._hoodies = []
+}
+
+get listOfHoodies(): IContent[]{
+    return this._hoodies
+}
+
+public addContent(hoodiesToAdd: IContent){
+    this._hoodies.push(hoodiesToAdd)
+}
+
+public arrayLength(){
+    this._hoodies.length
+}
+
+
+
+
+
 }
